ec719a6ae5a7f94a5a99b9dfed0d1b309225158f
Allen / 임기원 <l4279625@gmail.com>
2024-05-24 15:01:27 +0900

    [level 1] Title: 공원 산책, Time: 0.14 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -6,9 +6,6 @@ class Solution {
     Map<String, Integer> dCol = Map.of("E",1, "W",-1, "S",0, "N",0);
     
     public int[] solution(String[] park, String[] routes) {
-        int rowMax = park.length;
-        int colMax = park[0].length();
-        
         int[] current = getStartCoordinate(park);
         
         for (String routeStr : routes) {
@@ -18,17 +15,17 @@ class Solution {
         return current;
     }
     
+    // 시작지점 좌표 탐색
     private int[] getStartCoordinate(String[] park) {
         int rowMax = park.length;
-        for (int row = 0; row < rowMax; row++) {
+        for (int row = 0; row < park.length; row++) {
             int col = park[row].indexOf('S');
-            if (col != -1) {
-                return new int[]{ row, col };
-            }
+            if (col != -1) return new int[]{ row, col };
         }
         return null;
     }
     
+    // 좌표 이동
     private void move(int[] current, String routeStr, String[] park) {
         String[] route = routeStr.split(" ");
         int rowDirection = dRow.get(route[0]);
@@ -37,17 +34,21 @@ class Solution {
         int distance = Integer.parseInt(route[1]);
         
         int movedRow = current[0] + rowDirection * distance;
+        // 유효한 row 이동인지 체크
         if (movedRow < 0 || movedRow >= park.length) return;
         
         int movedCol = current[1] + colDirection * distance;
+        // 유효한 col 이동인지 체크
         if (movedCol < 0 || movedCol >= park[movedRow].length()) return;
         
+        // 한칸씩 이동하면서 장애물에 막히는지 체크
         for (int i=1; i<=distance; i++) {
             int row = current[0] + (rowDirection * i);
             int col = current[1] + (colDirection * i);
             if (park[row].charAt(col) == 'X') return;
         }
         
+        // 유효한 이동이고 장애물에도 막히지 않으면 좌표 이동
         current[0] = movedRow;
         current[1] = movedCol;
     }
