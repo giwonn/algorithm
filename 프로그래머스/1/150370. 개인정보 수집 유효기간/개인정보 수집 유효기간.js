38a3d69a90dc08bb2949334fc83d1db8b8277779
Allen / 임기원 <l4279625@gmail.com>
2024-05-29 00:53:16 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.57 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,48 +1,19 @@
 function solution(today, terms, privacies) {
     const termMap = terms.reduce((obj, term) => {
         const [str, num] = term.split(' ')
-        obj[str] = Number(num)
+        obj[str] = Number(num) // ex) { 'A' : 6 }
         return obj
     }, {})
     
     return privacies.reduce((arr, privacy, index) => {
         const [date, str] = privacy.split(' ')
-        const expireDate = getExpireDate(date, termMap[str])
-        if (isAfter(today, expireDate)) arr.push(index+1)
+        const expireTime = getTime(date) + termMap[str] * 28
+        if (expireTime <= getTime(today)) arr.push(index+1)
         return arr
     }, [])
 }
 
-function getExpireDate(date, afterMonth) {
+function getTime(date) {
     const [year, month, day] = date.split('.').map(Number)
-    
-    const calculateMonth = month + afterMonth
-    
-    let resultDay = day - 1
-    
-    let resultMonth = calculateMonth % 12
-    if (resultDay === 0) resultMonth--
-    
-    let resultYear = Number(year) + Math.floor(calculateMonth / 12)
-    if (resultMonth === 0) resultYear--
-    
-    if (resultDay === 0) resultDay = 28
-    if (resultMonth === 0) resultMonth = 12
-    
-    resultDay = resultDay < 10 ? `0${resultDay}` : resultDay.toString()
-    resultMonth = resultMonth < 10 ? `0${resultMonth}` : resultMonth.toString()
-    resultYear = resultYear.toString()
-    
-    return `${resultYear}.${resultMonth}.${resultDay}`
-}
-
-function isAfter(aDate, bDate) {
-    const [aYear, aMonth, aDay] = aDate.split('.').map(Number)
-    const [bYear, bMonth, bDay] = bDate.split('.').map(Number)
-    
-    if (aYear < bYear) return false
-    if (aYear === bYear && aMonth < bMonth) return false
-    if (aYear === bYear && aMonth === bMonth && aDay <= bDay) return false
-    
-    return true
+    return year * 12 * 28 + month * 28 + day
 }
