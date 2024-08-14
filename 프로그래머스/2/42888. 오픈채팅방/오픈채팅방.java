64a8628e2e1485a0763f8b60b7e89196d45eeddb
Allen / 임기원 <l4279625@gmail.com>
2024-08-14 10:32:29 +0900

    [level 2] Title: 오픈채팅방, Time: 136.60 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
 	public static String[] solution(String[] records) {
-		Map<String, String> nickname = getUserNickname(records);
-
 		List<String> answer = new ArrayList<>();
-		for (String record: records) {
-			String[] arr = record.split(" ");
-			if (arr[0].equals("Change")) continue;
+		Map<String, String> userNicknames = getUserNicknames(records);
 
-			String message = arr[0].equals("Enter") ? "님이 들어왔습니다." : "님이 나갔습니다.";
-			answer.add(nickname.get(arr[1]) + message);
+		for (String record: records) {
+			if (record.startsWith("Change")) continue;
+			String[] arr = record.split(" ");
+			answer.add(userNicknames.get(arr[1]) + (arr[0].equals("Enter") ? "님이 들어왔습니다." : "님이 나갔습니다."));
 		}
 
 		return answer.toArray(new String[0]);
 	}
 
-	public static Map<String, String> getUserNickname(String[] records) {
+	public static Map<String, String> getUserNicknames(String[] records) {
 		Map<String, String> userNickname = new HashMap<>();
 
 		for (String record: records) {
+			if (record.startsWith("Leave")) continue;
 			String[] arr = record.split(" ");
-			if (arr[0].equals("Leave")) continue;
 			userNickname.put(arr[1], arr[2]);
 		}
 
