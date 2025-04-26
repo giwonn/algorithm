7d2e7706ed419e491121d5e228abcd66b5a451d2
Allen / 임기원 <l4279625@gmail.com>
2025-04-26 16:10:15 +0900

    [level 3] Title: 입국심사, Time: 57.44 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -5,12 +5,10 @@ class Solution {
     // 1 <= times <= 1_000_000_000
     // 1 <= times.length <= 100_000
     public long solution(int n, int[] times) {
-        long answer = 0;
-        
         long left = 0;
         long right = Arrays.stream(times).max().getAsInt() * (long) n;
         
-        while (left <= right) {
+        while (left < right) {
             long mid = (left + right) / 2;
             long completeN = 0;
             
@@ -21,11 +19,10 @@ class Solution {
             if (completeN < n) { // n명을 심사하는데 시간 mid로는 부족한 경우
                 left = mid + 1;
             } else {
-                right = mid - 1;
-                answer = mid; // 다음 루프의 mid가 시간을 초과할 경우에 이전 루프의 mid가 최적의 해이기 때문에 미리 저장
+                right = mid;
             }
         }
         
-        return answer;
+        return right;
     }
 }
