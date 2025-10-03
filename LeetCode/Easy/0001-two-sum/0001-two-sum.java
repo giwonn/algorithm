76b5b343826aa9d48b9dc443db8fe9826ecd2f8e
Allen / 임기원 <l4279625@gmail.com>
2025-10-03 15:32:59 +0900

    Time: 45 ms (30.16%), Space: 45.1 MB (31.93%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -3,7 +3,6 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    System.out.println(i + " " + j);
                     return new int[]{i, j};
                 }
             }
